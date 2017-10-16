928244cb9375a77256b9ab7e817adea7d135e360
Sean Hite <seanhite@gmail.com>
2017-10-16 18:43:39 -0400

    checkpoints 2, 3, 4

--- lib/anton.rb
+++ lib/anton.rb
@@ -6,10 +6,20 @@ class Anton
 
   def initialize(email, password)
     # make an API call to your Bloc API endpoint with your actual Bloc email and password  
-    response = self.class.post(base_api_endpoint("sessions"), body: { "seanhite@gmail.com": email, "Ymca1234": password })
+    response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
     # if successful, you will get a token
     @auth_token = response["auth_token"]
   end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_data = JSON.parse(response.body)
+  end  
 
 private
 
